cc6ea5d11abc8caf3fb863e5ad6d4e06ccda430e
ChillerDragon <chillerdragon@gmail.com>
2025-08-29 12:37:46 +0200

    things

--- gaghob.js
+++ gaghob.js
@@ -30,13 +30,37 @@ const isGithubPullsPage= () => {
   return false
 }
 
+const annotatePr = (prId) => {
+  ghApi(`/repos/ddnet/ddnet/pulls/${prId}`).then((data) => {
+    // console.log(data.rebaseable)
+    // console.log(data)
+
+    if(data.rebaseable) {
+      console.log(`pr ${prId} is rebaseable`)
+    }
+  })
+}
+
+const setPrIcon = (prDiv) => {
+  const iconDiv = prDiv.querySelector('commit-build-statuses')
+  iconDiv.innerHTML = '<h1>HELLO</h1>'
+}
+
+const listPrs = () => {
+  const prDivs = document.querySelectorAll('[data-issue-and-pr-hovercards-enabled][aria-label="Issues"] > div > div')
+  prDivs.forEach((prDiv) => {
+    // const prId = prDiv.id.split('_')[1]
+    setPrIcon(prDiv)
+  })
+}
+
 const enrichPulls = () => {
   if(!isGithubPullsPage()) {
     return
   }
 
-  // does auth work?
-  ghApi('/repos/ddnet/ddnet/pulls/10773').then((d) => console.log(d))
+  listPrs()
+  // annotatePr(10773)
 }
 
 enrichPulls()
